11b5d016615261bc9d4c68c13886c591e0cc2c7a
Mathias <Mathias@LAPTOP-84H7VQ2I>
2016-10-06 09:25:17 +0200

    Add project files.

---
+++ 12_Employee/Employee.cs
@@ -0,0 +1,47 @@
+﻿namespace _12_Employee
+{
+    public class Employee
+    {
+        private int id;
+        private string name;
+        private string type;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public Employee(string name, string type, int id)
+        {
+            this.name = name;
+            this.type = type;
+            this.id = id;
+        }
+
+        public override string ToString()
+        {
+            return "ID=" + id + "_NAME=" + name + "_TYPE=" + type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.ToString() == ToString())
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ 12_Employee/EmployeeRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _12_Employee
+{
+    public class EmployeeRepository
+    {
+        List<Employee> employeeList = new List<Employee>();
+        int id = 0;
+
+        public void Clear()
+        {
+            employeeList.Clear();
+        }
+
+        public int CountEmployees()
+        {
+            return employeeList.Count;
+        }
+
+        public Employee CreateEmployee(string name, string type)
+        {
+            id++;
+            employeeList.Add(new Employee(name, type, id));
+            return new Employee(name, type, id);
+        }
+
+        public void SaveEmployee(Employee employee)
+        {
+            int currentIndex = employee.Id - 1;
+
+            employeeList[employee.Id - 1].Name = employee.Name;
+            employeeList[employee.Id - 1].Type = employee.Type;
+        }
+
+        public Employee LoadEmployee(int id)
+        {
+            return employeeList.Where(employee => employee.Id == id).FirstOrDefault();
+        }
+
+        public List<Employee> FindAllEmployees()
+        {
+            return employeeList;
+        }
+    }
+}
